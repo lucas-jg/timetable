517c830001b8d105dcabb1938cee8370b0c078f8
lucas <kjk727@me.com>
2020-01-22 11:44:13 +0900

    Styled-component에 props를 넣어 동적으로 스타일 적용

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+interface StyledProps {
+    danger: boolean
+}
 
 const Header = styled.header`
     width: 1280px;
     height: 60px;
     border: 1px solid black;
     border-radius: 2px;
+    ${(props: StyledProps) => {
+        if (props.danger) {
+            return css`
+                background: #efefefef;
+            `
+        }
+    }}
 `
 
 const Footer = styled.footer`
@@ -18,7 +29,7 @@ const Footer = styled.footer`
 
 const Layout: React.FC = ({ children }) => (
     <div className='HeaderWrapper'>
-        <Header>Header</Header>
+        <Header danger={false}>Header</Header>
         {children}
         <Footer>Footer</Footer>
     </div>
